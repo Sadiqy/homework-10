7771c9553447f1996700dd24f66b3252e0431c59
Git Sadiq <abdulaisadiq@live.com>
2020-02-23 18:44:05 -0800

    Created parent classes and completed templates

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,22 @@
+class Employee {
+    constructor(name, id, email, role){
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.role = role || 'Employee'
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getRole() {
+        return this.role;
+    }
+}
+
+module.exports = Employee;
